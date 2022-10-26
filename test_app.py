c5d5f401c8b059ffed1fa34e9420386b348955fc
Sacha LASRY <sacha.lasry@artefact.com>
2022-10-26 20:15:28 +0200

    feat: Added test requirements txxt file to test it for deployment

--- test_app.py
+++ test_app.py
@@ -19,22 +19,13 @@ def load_data(nrows):
     return data
 
 
-data_load_state = st.text('Loading data...')
-
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    st.write("HELLO")
 
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.write(hour_to_filter)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+st.subheader('Text search bar test')
+selected = st.text_input("", "Search...")
