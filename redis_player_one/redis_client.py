4ddc49ffb4a76954e709aeb3e58ce6cedeebf033
Karim SI LARBI <karim.si-larbi@artefact.com>
2022-10-29 09:11:13 +0200

    feature/ minimal version of similarity search

--- redis_player_one/redis_client.py
+++ redis_player_one/redis_client.py
@@ -1,5 +1,5 @@
 import redis
-from config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+from redis_conf import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
@@ -10,10 +10,12 @@ def retrieve_paper(r_client, key):
     title = r_client.hget(key, "title")
     authors = r_client.hget(key, "authors")
     year = r_client.hget(key, "year")
+    paper_id = r_client.hget(key, "paper_id")
 
     return {
         "abstract": abstract,
         "title": title,
         "authors": authors,
         "year": year,
+        "paper_id": paper_id
     }
