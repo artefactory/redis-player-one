0859c3cadcd8122e9dc14bb89487d15aff5ff87a
Hugo VASSELIN <hugo.vasselin@artefact.com>
2022-11-03 10:55:18 +0100

    feat: add optims

--- frontend_streamlit/streamlit_app.py
+++ frontend_streamlit/streamlit_app.py
@@ -16,17 +16,14 @@ from redis_player_one.embedder import make_embeddings
 from redis_player_one.redis_client import redis_client
 
 
-document_store = RedisDocumentStore(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
-retriever = BM25Retriever(document_store=document_store)
-reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False, context_window_size=2000)
-pipe = ExtractiveQAPipeline(reader, retriever)
-
-
-st.set_page_config(page_title="Redis Player One",
-                   page_icon="https://cdn4.iconfinder.com/data/icons/redis-2/1451/Untitled-2-512.png",
-                   layout='wide')
-
-st.sidebar.title('Redis Player One - Similarity Search Engine')
+@st.experimental_singleton(show_spinner=False)
+def instanciate_retriever():
+    with st.spinner("Loading models..."):
+        document_store = RedisDocumentStore(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+        retriever = BM25Retriever(document_store=document_store)
+        reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False, context_window_size=2000)
+        pipe = ExtractiveQAPipeline(reader, retriever)
+    return pipe
 
 
 def create_query(
@@ -93,64 +90,7 @@ def plot_results(results):
     st.line_chart(values_to_plot, x="year", y="similarity_score")
 
 
-# def app():
-#     user_text = st.sidebar.text_input(label="Enter some text here 👇", value="", max_chars=2000, key="user_text_input")
-#     nb_articles = st.sidebar.number_input("Insert the number of simillar articles to retrieve", step=1, min_value=0)
-#     date_range = st.sidebar.slider('Select a range of dates', 2015, 2022, (2016, 2019))
-#     clicked = st.sidebar.button('Submit')
-#     if clicked and user_text and nb_articles > 0:
-#         st.markdown(f'<h2 style="color:#FFFFFF;font-size:30px;">You\'ve entered: <br><em>\"{user_text}\"</em></h1>',
-#                     unsafe_allow_html=True)
-#         st.markdown("""---""")
-
-#         with st.spinner("Computing similarity research"):
-#             start_time = time.time()
-#             results = submit_text(
-#                 text=user_text,
-#                 date_range=list(map(str, list((range(date_range[0], date_range[1] + 1))))),
-#                 nb_articles=nb_articles
-#             )
-#             end_time = time.time()
-#         st.sidebar.success(f"Found {nb_articles} abstracts in {round(end_time - start_time, 2)} seconds!")
-#         if results:
-#             for i, paper in enumerate(results.docs):
-#                 col1, col2 = st.columns([3, 1])
-#                 with col1:
-#                     st.markdown(f'<h2 style="color:#2892D7;font-size:24px;">Abstract #{i + 1} - {paper.title}</h1>',
-#                                 unsafe_allow_html=True)
-#                     st.write(paper.abstract)
-#                 with col2:
-#                     similarity_score_str = f"{round(100*(1 - float(paper.vector_score)), 1)}%"
-#                     st.markdown('<h2 style="color:#F71735;font-size:24px;"><u>Similarity score:</u></h2>',
-#                                 unsafe_allow_html=True)
-#                     st.markdown(f'<h2 style="color:#FFFFFF;font-size:24px;">📊 {similarity_score_str}</h2>',
-#                                 unsafe_allow_html=True)
-
-#                     st.markdown('<h1 style="color:#F71735;font-size:16px;"><u>Link to the article</u></h1>',
-#                                 unsafe_allow_html=True)
-#                     st.write(f"🔗 https://arxiv.org/abs/{paper.paper_id}")
-
-#                     if paper.update_date:
-#                         st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Updated on:</u></h1>',
-#                                     unsafe_allow_html=True)
-#                         st.markdown(f'<h1 style="color:#FFFFF;font-size:14px;">📆 {paper.update_date}</h1>',
-#                                     unsafe_allow_html=True)
-
-#                     if paper.categories:
-#                         cats_list = paper.categories.split(",")
-#                         def_list = sorted(set(map(lambda x: CAT_TO_DEFINITION_MAP.get(x, x), cats_list)))
-#                         def_list[0] = f'✅ {def_list[0]}'
-#                         def_str = '<br>✅ '.join(def_list)
-#                     else:
-#                         def_str = "Unknown categories"
-#                     st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Categories:</u></h1>',
-#                                 unsafe_allow_html=True)
-#                     st.markdown(f'<h1 style="color:#FFFFFF;font-size:14px;">{def_str}</h1>',
-#                                 unsafe_allow_html=True)
-#                 st.markdown("""---""")
-
-
-def submit_text(text: str, date_range: list, nb_articles: int):
+def submit_text(pipe, text: str, date_range: list, nb_articles: int):
     results = pipe.run(
         query=text,
         params={
@@ -160,75 +100,98 @@ def submit_text(text: str, date_range: list, nb_articles: int):
     return results
 
 
-def app_qa():
+def button_callback(name: str) -> None:
+    """Callback to change the session_state of the button to True
+    Args:
+        name (str): button name
+    """
+    st.session_state[name] = True
 
-    # print_documents(prediction, max_text_len=200)
-    # print_answers(prediction, max_text_len=200)
 
-    user_question = st.sidebar.text_input(label="Enter your question here 👇", value="", max_chars=2000, key="user_question_input")
-    nb_articles = st.sidebar.number_input("Insert the number of simillar articles to retrieve", step=1, min_value=0)
-    date_range = st.sidebar.slider('Select a range of dates', 2015, 2022, (2016, 2019))
-    clicked = st.sidebar.button('Submit')
-    if clicked and user_question and nb_articles > 0:
-        st.markdown(f'<h2 style="color:#FFFFFF;font-size:30px;">You\'ve entered: <br><em>\"{user_question}\"</em></h1>',
-                    unsafe_allow_html=True)
-        st.markdown("""---""")
+def instanciate_button(button_name: str) -> None:
+    if button_name not in st.session_state:
+        st.session_state[button_name] = False
 
-        with st.spinner("Computing similarity research"):
-            start_time = time.time()
-            results = submit_text(
-                text=user_question,
-                date_range=list(map(str, list((range(date_range[0], date_range[1] + 1))))),
-                nb_articles=nb_articles
-            )
-            end_time = time.time()
-        st.sidebar.success(f"Found {len(results['answers'])} abstracts in {round(end_time - start_time, 2)} seconds!")
-        if results:
-            answers = sorted(results["answers"], key=lambda x: x.score, reverse=False)
-            for i, paper in enumerate(answers):
-                col1, col2 = st.columns([3, 1])
-                with col1:
-                    st.markdown(f'<h2 style="color:#2892D7;font-size:24px;">Abstract #{i + 1} - {paper.meta["title"]}</h1>',
-                                unsafe_allow_html=True)
-                    abstact_str = paper.context
-                    start, end = paper.offsets_in_document[0].start, paper.offsets_in_document[0].end
 
-                    abstact_str = f'{abstact_str[:start]}<b style="background-color:#FFFF00;color:#4C4C4C;">{abstact_str[start:end]}</b>{abstact_str[end:]}'
+def app():
+    st.set_page_config(page_title="Redis Player One",
+                   page_icon="https://arxiv.org/favicon.ico",
+                   layout='wide')    
+    pipe = instanciate_retriever()
+    instanciate_button("button1")
+    st.sidebar.title('Redis Player One - Similarity Search Engine')
+    with st.form(key="content_section"):
+        with st.sidebar:
+            user_question = st.text_input(label="Enter your question here 👇", max_chars=2000, key="user_question_input")
+            nb_articles = st.number_input(
+                label="Insert the number of simillar articles to retrieve", value=10, step=1, min_value=1)
+            date_range = st.slider('Select a range of dates', 2015, 2022, (2016, 2019))    # TODO: avoid hardcoding the date range if possible
+            st.form_submit_button('Submit', on_click=button_callback, kwargs={"name": "button1"})
 
-                    st.markdown(f'<p style="color:#FFF;font-size:24px;">{abstact_str}</p>',
-                                unsafe_allow_html=True)
+    if st.session_state["button1"]:
+        if not user_question:
+            st.error('please type a question in the searchbar')
+        else:
+            st.markdown(f'<h2 style="color:#FFFFFF;font-size:30px;">You\'ve entered: <br><em>\"{user_question}\"</em></h1>',
+                        unsafe_allow_html=True)
+            st.markdown("""---""")
 
-                with col2:
-                    similarity_score_str = f"{round(100 *(1 - float(paper.score)), 1)}%"
-                    st.markdown('<h2 style="color:#F71735;font-size:24px;"><u>Similarity score:</u></h2>',
-                                unsafe_allow_html=True)
-                    st.markdown(f'<h2 style="color:#FFFFFF;font-size:24px;">📊 {similarity_score_str}</h2>',
-                                unsafe_allow_html=True)
-
-                    st.markdown('<h1 style="color:#F71735;font-size:16px;"><u>Link to the article</u></h1>',
-                                unsafe_allow_html=True)
-                    st.write(f"🔗 https://arxiv.org/abs/{paper.document_id}")
-
-                    if paper.meta["update_date"]:
-                        st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Updated on:</u></h1>',
+            with st.spinner("Computing similarity research"):
+                start_time = time.time()
+                results = submit_text(
+                    pipe=pipe,
+                    text=user_question,
+                    date_range=list(map(str, list((range(date_range[0], date_range[1] + 1))))),
+                    nb_articles=nb_articles
+                )
+                end_time = time.time()
+            st.sidebar.success(f"Found {len(results['answers'])} abstracts in {round(end_time - start_time, 2)} seconds!")
+            if results:
+                answers = sorted(results["answers"], key=lambda x: x.score, reverse=False)
+                for i, paper in enumerate(answers):
+                    col1, col2 = st.columns([3, 1])
+                    with col1:
+                        st.markdown(f'<h2 style="color:#2892D7;font-size:24px;">Abstract #{i + 1} - {paper.meta["title"]}</h1>',
                                     unsafe_allow_html=True)
-                        st.markdown(f'<h1 style="color:#FFFFF;font-size:14px;">📆 {paper.meta["update_date"]}</h1>',
+                        abstact_str = paper.context
+                        start, end = paper.offsets_in_document[0].start, paper.offsets_in_document[0].end
+
+                        abstact_str = f'{abstact_str[:start]}<b style="background-color:#FFFF00;color:#4C4C4C;">{abstact_str[start:end]}</b>{abstact_str[end:]}'
+
+                        st.markdown(f'<p style="color:#FFF;font-size:24px;">{abstact_str}</p>',
                                     unsafe_allow_html=True)
 
-                    if paper.meta["categories"]:
-                        cats_list = paper.meta["categories"].split(",")
-                        def_list = sorted(set(map(lambda x: CAT_TO_DEFINITION_MAP.get(x, x), cats_list)))
-                        def_list[0] = f'✅ {def_list[0]}'
-                        def_str = '<br>✅ '.join(def_list)
-                    else:
-                        def_str = "Unknown categories"
-                    st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Categories:</u></h1>',
-                                unsafe_allow_html=True)
-                    st.markdown(f'<h1 style="color:#FFFFFF;font-size:14px;">{def_str}</h1>',
-                                unsafe_allow_html=True)
-                st.markdown("""---""")
+                    with col2:
+                        similarity_score_str = f"{round(100 *(1 - float(paper.score)), 1)}%"
+                        st.markdown('<h2 style="color:#F71735;font-size:24px;"><u>Similarity score:</u></h2>',
+                                    unsafe_allow_html=True)
+                        st.markdown(f'<h2 style="color:#FFFFFF;font-size:24px;">📊 {similarity_score_str}</h2>',
+                                    unsafe_allow_html=True)
+
+                        st.markdown('<h1 style="color:#F71735;font-size:16px;"><u>Link to the article</u></h1>',
+                                    unsafe_allow_html=True)
+                        st.write(f"🔗 https://arxiv.org/abs/{paper.document_id}")
+
+                        if paper.meta["update_date"]:
+                            st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Updated on:</u></h1>',
+                                        unsafe_allow_html=True)
+                            st.markdown(f'<h1 style="color:#FFFFF;font-size:14px;">📆 {paper.meta["update_date"]}</h1>',
+                                        unsafe_allow_html=True)
+
+                        if paper.meta["categories"]:
+                            cats_list = paper.meta["categories"].split(",")
+                            def_list = sorted(set(map(lambda x: CAT_TO_DEFINITION_MAP.get(x, x), cats_list)))
+                            def_list[0] = f'✅ {def_list[0]}'
+                            def_str = '<br>✅ '.join(def_list)
+                        else:
+                            def_str = "Unknown categories"
+                        st.markdown('<h1 style="color:#F71735;font-size:14px;"><u>Categories:</u></h1>',
+                                    unsafe_allow_html=True)
+                        st.markdown(f'<h1 style="color:#FFFFFF;font-size:14px;">{def_str}</h1>',
+                                    unsafe_allow_html=True)
+                    st.markdown("""---""")
 
 
 if __name__ == "__main__":
-    app_qa()
+    app()
 
